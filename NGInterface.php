1ffa4bfd25fdc6988090e52c06fd68c4b33afd8b
Ultreia- <201304196@post.au.dk>
2015-05-05 23:28:53 +0200

    MERE INDHOLD #300LINJERTILFOLKET

--- NGInterface.php
+++ NGInterface.php
@@ -98,8 +98,54 @@
 						<td colspan="6" class="hiddenRow">
 							<div class="accordian-body collapse" id="collapsableContent-9435"> 
 								<div class="bs-callout bs-callout-warning">
-  									<h3>Ordre 9435</h3>
-  									
+  									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div> 
 							</div> 
 						</td>
@@ -120,7 +166,54 @@
 							<div class="accordian-body collapse" id="collapsableContent-9432"> 
 								<div class="bs-callout bs-callout-warning">
   									<h3>Ordre 9432</h3>
-  									
+  									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div>
 							</div>
 						</td>
@@ -157,6 +250,54 @@
 							<div class="accordian-body collapse" id="collapsableContent-9429">
 								<div class="bs-callout bs-callout-success">
   									<h3>Ordre 9429</h3>
+  									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div>
 							</div> 
 						</td>
@@ -177,6 +318,54 @@
 							<div class="accordian-body collapse" id="collapsableContent-9433">
 								<div class="bs-callout bs-callout-success">
   									<h3>Ordre 9433</h3>
+  									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div>
 							</div> 
 						</td>
@@ -211,6 +400,54 @@
 							<div class="accordian-body collapse" id="collapsableContent-9436">
 								<div class="bs-callout bs-callout-danger">
 									<h3>Ordre 9436</h3>
+									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div>
 							</div> 
 						</td>
@@ -231,6 +468,54 @@
 							<div class="accordian-body collapse" id="collapsableContent-9437">
 								<div class="bs-callout bs-callout-danger">
   									<h3>Ordre 9437</h3>
+  									<div class="row">
+	  									<div class="col-xs-6">
+		  									
+		  									<h4>Produkt</h4>
+		  										<h5>Plakat</h5>
+		  									<h4>Format</h4>
+		  										<h5>A3</h5>
+		  										<h5>Type: Tosidet print</h5>
+		  									<h4>Brug</h4>
+		  										<h5>Indendørs</h5>
+		  										<h5>Materiale: Skumpap</h5>
+		  									<h4>Beskæring</h4>
+		  										<h5>Beskæringskant: Ja</h5>
+		  										<h5>Skæremærker: Nej</h5>
+		  									<h4>Filer</h4>
+		  										<h5>1 fil uploadet til:</h5>
+		  										<h5><b>C:/Users/Henrik/Dropbox/Delemappe/Plakat.pdf</b></h5>
+
+			 						 	</div>
+
+			 						 	<div class="col-xs-6">
+		 						 		
+				 						 	<h4>Antal</h4>
+				 						 		<h5>4</h5>
+				 						 	<h4>Leveringstidspunkt</h4>
+				 						 		<h5>14/5/2015 kl. 14:00</h5>
+				 						 	<h4>Fakturering</h4>
+				 						 		<h5>Rasmus Kjeldgaard</h5>
+				 						 		<h5>Fjældevænget 112, 2. tv</h5>
+				 						 		<h5>8210, Aarhus V</h5>
+				 						 	<h4>Levering</h4>
+				 						 		<h5>Afhentes af kunde</h5>	
+
+		 						 		</div>	
+		 						 	</div>
+									<div class="row">
+
+	  									<button type="button" class="btn btn-default btn-lg pull-right declineorder-btn">
+					  						Afvis ordre
+										</button>
+										<button type="button" class="btn btn-default btn-lg pull-right acceptorder-btn">
+					  						Godkend ordre
+										</button>
+										<a class="btn btn-default btn-lg pull-right contactcustomer-btn" href="mailto:201304196@post.au.dk" role="button">
+										Kontakt kunde
+										</a>
+
+									</div>
 								</div>
 							</div> 
 						</td>
