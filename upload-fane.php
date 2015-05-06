e42ab181fcec1d431fef5a5f48a114aeda01fc8f
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-06 21:24:11 +0200

    upload fane v2

--- upload-fane.php
+++ upload-fane.php
@@ -2,6 +2,97 @@
 
 <body>
 	
+	<div class="container bs-docs-container">
+		
+	<h2>Har du husket beskæring?</h2>
 	
+	<p>
+		Nulla mollis urna at felis maximus, at sollicitudin velit gravida. Nulla lacus ipsum, aliquet vel enim ac, varius vulputate felis. Morbi id ligula elit. Nam sed scelerisque dui, non maximus ligula. Cras sed interdum nibh. Nunc nec consequat lectus, porta lobortis enim. Suspendisse eget enim porta, blandit eros ac, imperdiet sem. Donec rhoncus felis purus, vel pretium lectus ultricies et. Integer ut elementum felis. Suspendisse tincidunt sem tincidunt elit mattis, id lobortis ante viverra. Vestibulum ultricies libero vel cursus dignissim. Pellentesque eu lobortis enim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vel neque in risus blandit vulputate. Suspendisse et pulvinar enim. In ultrices commodo eleifend.
+	</p>
+	
+	<img class="pull-left" src="img/new-document-settings-indesign.png" alt="New document settings indesign" class="img-rounded">
+	<img class="pull-right" src="img/bleed-edge.png" alt="Dokument bleed edge" class="img-rounded">
+
+	<p>
+		Nam sem magna, consectetur quis tortor sed, dapibus pellentesque leo. Aliquam aliquam ligula vitae eleifend tincidunt. Aenean mattis felis sit amet nunc tempor tempor. Cras non lacus nisi. Phasellus aliquet augue ligula, eget laoreet orci pulvinar ac. Suspendisse id lorem tincidunt, tristique quam id, auctor tortor. In mauris turpis, sollicitudin ut venenatis et, mollis at enim.
+	</p>
+
+	<p>
+		Morbi sollicitudin nec nunc at facilisis. Ut vehicula lorem erat, a consequat diam sagittis eu. Sed sed massa ligula. Morbi vel lacinia leo. Aenean porta mauris ligula, ac condimentum elit euismod eget. In sed molestie dolor. Nunc nisi sem, feugiat at orci malesuada, accumsan sollicitudin risus. Donec quis ipsum eros. Aenean vitae consequat risus. Aenean id nisi vel leo porttitor sagittis nec quis tellus. Pellentesque vitae pellentesque lacus. Nam vitae sagittis elit, eget semper velit. Praesent non dui neque. Proin et mi eu purus eleifend semper a non sem. Cras a viverra ligula.
+	</p>
+
+	<p>
+		Proin accumsan metus a dui consectetur, non fringilla neque hendrerit. Donec eget dignissim est, gravida convallis arcu. Aliquam sed enim condimentum, hendrerit ex quis, feugiat mi. Vivamus est nisl, lobortis suscipit tristique sit amet, pharetra pretium lectus. Cras sollicitudin felis dui, et dapibus erat feugiat et. Vestibulum tellus justo, sagittis et lobortis ac, facilisis sit amet nibh. Proin auctor ut arcu eu pulvinar. Sed leo libero, consequat nec nunc facilisis, pretium feugiat urna. Curabitur non ipsum magna. Sed accumsan lorem dolor, sed pellentesque leo venenatis ut. Sed non porttitor elit, sit amet commodo arcu. Nullam eu vulputate nisl, rutrum molestie tellus. Donec tincidunt auctor diam, ac hendrerit arcu interdum lobortis. Mauris vitae lorem massa. Donec faucibus cursus mi sit amet placerat.
+	</p>
+
+	
+		<div>
+			
+			<div class="checkbox">
+			
+				<label>
+			
+					<input type="radio" id="checkboxSuccess" name="bleed" value="option1">
+					Ja!
+			
+				</label>
+			
+			</div>
+		
+		</div>
+		
+		<div>
+			
+			<div class="checkbox">
+				
+				<label>
+					
+					<input type="radio" id="checkboxWarning" name="bleed" value="option1">
+					Nej, jeg behøver hjælp til det.
+					
+				</label>
+			
+			</div>
+		
+		</div>
+		
+	</div>
+
+	<h3>Upload dine filer</h3>
+
+	<button type="button" class="btn btn-default btn-lg">Vælg filer</button>
+	<button type="button" class="btn btn-default btn-lg">Start upload</button>
+
+	<div class="media">
+	
+		<div class="media-left">
+		
+			<img class="media-object" style="width: 100px; height: 100px; " src="img/plakat1986.jpg" alt="Test upload">
+		
+		</div>
+
+		<div class="media-body">
+		
+			<h4 class="media-heading">festivals-poster.pdf</h4>
+
+			<p>
+				46MB
+				<br>
+				PDF
+			</p>
+
+			<div class="progress">
+				
+				<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+					60%
+				</div>
+				
+			</div>
+
+		</div>
+	
+	</div>
+
+	</div>
 
 </body>
