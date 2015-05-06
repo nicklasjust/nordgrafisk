d8082384879fa9614f1a301286771a55a11e69de
nicklasjust <nicklas_just2@hotmail.com>
2015-05-06 23:24:49 +0200

    format-fane

---
+++ format-fane.php
@@ -0,0 +1,89 @@
+<div role="tabpanel" class="tab-pane" id="format">
+				    	
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+	Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	<div class="container">
+		<div class="row">
+			<h3>Størrelse:</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="dropdown">
+			  		<button class="btn btn-default dropdown-toggle" type="button" id="size-dropdown" data-toggle="dropdown" aria-expanded="true">
+			    		Størrelse
+			    		<span class="caret"></span>
+			  		</button>
+			  		<ul class="dropdown-menu" role="menu" aria-labelledby="size-dropdown">
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A0</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A1</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A2</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A3</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A4</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">A5</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B0</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B1</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B2</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B3</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B4</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">B5</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Andet</a></li>
+					</ul>
+				</div>
+			</div> <!-- end of dropdown column-->
+			<div class="col-md-4">
+				<div class="col-md-5">
+					<input type="text" class="form-control" placeholder="Længde">
+				</div>
+				<div class="col-md-1">
+					<p class="text-center">X</p>
+				</div>
+				<div class="col-md-5">
+					<input type="text" class="form-control" placeholder="Bredde">
+				</div>
+			</div> <!-- end of andet-->
+		</div>
+		<div class="row">
+			<h3>Type:</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="dropdown">
+			  		<button class="btn btn-default dropdown-toggle" type="button" id="size-dropdown" data-toggle="dropdown" aria-expanded="true">
+			    		Type
+			    		<span class="caret"></span>
+			  		</button>
+			  		<ul class="dropdown-menu" role="menu" aria-labelledby="size-dropdown">
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Karton</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Silkepapir</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Tyndt m. mat overflade</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Tyndt med shine</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Banner</a></li>
+					</ul>
+				</div>
+			</div> <!-- end of dropdown column-->
+		</div>
+		<div class="row">
+			<h3>Huller/Clips:</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="dropdown">
+			  		<button class="btn btn-default dropdown-toggle" type="button" id="size-dropdown" data-toggle="dropdown" aria-expanded="true">
+			    		Huller/Clips
+			    		<span class="caret"></span>
+			  		</button>
+			  		<ul class="dropdown-menu" role="menu" aria-labelledby="size-dropdown">
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">0 huller u. clips</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">0 huller m. clips</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">2 huller u. clips</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">2 huller m. clips</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">4 huller u. clips</a></li>
+					    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">4 huller m. clips</a></li>
+					</ul>
+				</div>
+			</div> <!-- end of dropdown column-->
+		</div>
+	</div>			    	
+</div>
