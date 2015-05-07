6c5a91db53069648ff7061d0ccafffa2517df75a
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-07 09:41:35 +0200

    indsættelse af faner brug og upload

--- upload-fane.php
+++ upload-fane.php
@@ -5,32 +5,85 @@
 	<div class="container bs-docs-container">
 		
 	<h2>Har du husket beskæring?</h2>
+
+	<div class="row">
+		
+		<div class="col-md-6">
+			
+			<p>
+				Lorem ipsum dolor sit amet, vel ultrices sapien mus pulvinar elit quis, libero bibendum urna vivamus consectetuer aliquam leo, amet ultrices tristique parturient tempus orci, mollis consectetuer pede. Quam eget convallis. Justo ut faucibus consectetuer venenatis habitant libero, sit dui, cursus adipiscing eu, sollicitudin enim reprehenderit mauris, blandit praesent ut mauris.
+			</p>
+
+			<p>
+				Morbi eget sed consectetuer, varius rhoncus sit mauris suscipit, diam semper vivamus dolor vero. Sit eros ac mattis mauris ac donec. Orci a lectus metus, dolor pellentesque wisi in mauris fermentum, pede amet tincidunt suscipit vestibulum vitae, in imperdiet sed purus, luctus erat congue nulla consectetur fringilla lacus. Vestibulum scelerisque et donec.
+			</p>
+
+			
+
+			<p>
+				Morbi eget sed consectetuer, varius rhoncus sit mauris suscipit, diam semper vivamus dolor vero. Sit eros ac mattis mauris ac donec. Orci a lectus metus, dolor pellentesque wisi in mauris fermentum, pede amet tincidunt suscipit vestibulum vitae, in imperdiet sed purus, luctus erat congue nulla consectetur fringilla lacus. Vestibulum scelerisque et donec.
+			</p>
+
+			<p>
+				Proin accumsan metus a dui consectetur, non fringilla neque hendrerit. Donec eget dignissim est, gravida convallis arcu. Aliquam sed enim condimentum, hendrerit ex quis, feugiat mi. Vivamus est nisl, lobortis suscipit tristique sit amet, pharetra pretium lectus. Cras sollicitudin felis dui, et dapibus erat feugiat et. Vestibulum tellus justo, sagittis et lobortis ac, facilisis sit amet nibh. Proin auctor ut arcu eu pulvinar. Sed leo libero, consequat nec nunc facilisis, pretium feugiat urna. Curabitur non ipsum magna. Sed accumsan lorem dolor, sed pellentesque leo venenatis ut. Sed non porttitor elit, sit amet commodo arcu. Nullam eu vulputate nisl, rutrum molestie tellus. Donec tincidunt auctor diam, ac hendrerit arcu interdum lobortis. Mauris vitae lorem massa. Donec faucibus cursus mi sit amet placerat.
+			</p>
+
+		</div>
+			
+		<br>
+
+		<div class="col-md-6">
+
+			<img class="center-block" src="img/new-document-settings-indesign.png" alt="New document settings indesign" class="img-rounded">
+		
+		</div>
 	
-	<p>
-		Nulla mollis urna at felis maximus, at sollicitudin velit gravida. Nulla lacus ipsum, aliquet vel enim ac, varius vulputate felis. Morbi id ligula elit. Nam sed scelerisque dui, non maximus ligula. Cras sed interdum nibh. Nunc nec consequat lectus, porta lobortis enim. Suspendisse eget enim porta, blandit eros ac, imperdiet sem. Donec rhoncus felis purus, vel pretium lectus ultricies et. Integer ut elementum felis. Suspendisse tincidunt sem tincidunt elit mattis, id lobortis ante viverra. Vestibulum ultricies libero vel cursus dignissim. Pellentesque eu lobortis enim. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi vel neque in risus blandit vulputate. Suspendisse et pulvinar enim. In ultrices commodo eleifend.
-	</p>
+	</div>
+
+	<br>
+
+	<div class="row">
+		
+		
+		<div class="col-md-6">
+
+			<img class="center-block" src="img/bleed-edge.png" alt="Dokument bleed edge" class="img-rounded">
+		
+		</div>
+
+		<br>
+
+		<div class="col-md-6">
+			
+			<p>
+				Lorem ipsum dolor sit amet, vel ultrices sapien mus pulvinar elit quis, libero bibendum urna vivamus consectetuer aliquam leo, amet ultrices tristique parturient tempus orci, mollis consectetuer pede. Quam eget convallis. Justo ut faucibus consectetuer venenatis habitant libero, sit dui, cursus adipiscing eu, sollicitudin enim reprehenderit mauris, blandit praesent ut mauris.
+			</p>
+
+			<p>
+				Morbi eget sed consectetuer, varius rhoncus sit mauris suscipit, diam semper vivamus dolor vero. Sit eros ac mattis mauris ac donec. Orci a lectus metus, dolor pellentesque wisi in mauris fermentum, pede amet tincidunt suscipit vestibulum vitae, in imperdiet sed purus, luctus erat congue nulla consectetur fringilla lacus. Vestibulum scelerisque et donec.
+			</p>
+
+			<p>
+				Lorem ipsum dolor sit amet, vel ultrices sapien mus pulvinar elit quis, libero bibendum urna vivamus consectetuer aliquam leo, amet ultrices tristique parturient tempus orci, mollis consectetuer pede. Quam eget convallis. Justo ut faucibus consectetuer venenatis habitant libero, sit dui, cursus adipiscing eu, sollicitudin enim reprehenderit mauris, blandit praesent ut mauris.
+			</p>
+
+			<p>
+				Morbi eget sed consectetuer, varius rhoncus sit mauris suscipit, diam semper vivamus dolor vero. Sit eros ac mattis mauris ac donec. Orci a lectus metus, dolor pellentesque wisi in mauris fermentum, pede amet tincidunt suscipit vestibulum vitae, in imperdiet sed purus, luctus erat congue nulla consectetur fringilla lacus. Vestibulum scelerisque et donec.
+			</p>
+
+		</div>
 	
-	<img class="pull-left" src="img/new-document-settings-indesign.png" alt="New document settings indesign" class="img-rounded">
-	<img class="pull-right" src="img/bleed-edge.png" alt="Dokument bleed edge" class="img-rounded">
+	</div>
 
-	<p>
-		Nam sem magna, consectetur quis tortor sed, dapibus pellentesque leo. Aliquam aliquam ligula vitae eleifend tincidunt. Aenean mattis felis sit amet nunc tempor tempor. Cras non lacus nisi. Phasellus aliquet augue ligula, eget laoreet orci pulvinar ac. Suspendisse id lorem tincidunt, tristique quam id, auctor tortor. In mauris turpis, sollicitudin ut venenatis et, mollis at enim.
-	</p>
+	<br>
 
-	<p>
-		Morbi sollicitudin nec nunc at facilisis. Ut vehicula lorem erat, a consequat diam sagittis eu. Sed sed massa ligula. Morbi vel lacinia leo. Aenean porta mauris ligula, ac condimentum elit euismod eget. In sed molestie dolor. Nunc nisi sem, feugiat at orci malesuada, accumsan sollicitudin risus. Donec quis ipsum eros. Aenean vitae consequat risus. Aenean id nisi vel leo porttitor sagittis nec quis tellus. Pellentesque vitae pellentesque lacus. Nam vitae sagittis elit, eget semper velit. Praesent non dui neque. Proin et mi eu purus eleifend semper a non sem. Cras a viverra ligula.
-	</p>
-
-	<p>
-		Proin accumsan metus a dui consectetur, non fringilla neque hendrerit. Donec eget dignissim est, gravida convallis arcu. Aliquam sed enim condimentum, hendrerit ex quis, feugiat mi. Vivamus est nisl, lobortis suscipit tristique sit amet, pharetra pretium lectus. Cras sollicitudin felis dui, et dapibus erat feugiat et. Vestibulum tellus justo, sagittis et lobortis ac, facilisis sit amet nibh. Proin auctor ut arcu eu pulvinar. Sed leo libero, consequat nec nunc facilisis, pretium feugiat urna. Curabitur non ipsum magna. Sed accumsan lorem dolor, sed pellentesque leo venenatis ut. Sed non porttitor elit, sit amet commodo arcu. Nullam eu vulputate nisl, rutrum molestie tellus. Donec tincidunt auctor diam, ac hendrerit arcu interdum lobortis. Mauris vitae lorem massa. Donec faucibus cursus mi sit amet placerat.
-	</p>
-
+	<div class="jumbotron">
 	
 		<div>
 			
-			<div class="checkbox">
+			<div class="radio">
 			
-				<label>
+				<label class="lead">
 			
 					<input type="radio" id="checkboxSuccess" name="bleed" value="option1">
 					Ja!
@@ -43,9 +96,9 @@
 		
 		<div>
 			
-			<div class="checkbox">
+			<div class="radio">
 				
-				<label>
+				<label class="lead">
 					
 					<input type="radio" id="checkboxWarning" name="bleed" value="option1">
 					Nej, jeg behøver hjælp til det.
@@ -73,10 +126,10 @@
 
 		<div class="media-body">
 		
-			<h4 class="media-heading">festivals-poster.pdf</h4>
+			<h4 class="media-heading">forside-festival-flyer.pdf</h4>
 
 			<p>
-				46MB
+				26MB
 				<br>
 				PDF
 			</p>
@@ -85,6 +138,36 @@
 				
 				<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
 					60%
+				</div>
+				
+			</div>
+
+		</div>
+	
+	</div>
+
+	<div class="media">
+	
+		<div class="media-left">
+		
+			<img class="media-object" style="width: 100px; height: 100px;" src="img/bagside.jpg" alt="Test upload">
+		
+		</div>
+
+		<div class="media-body">
+		
+			<h4 class="media-heading">bagside-festival-flyer.pdf</h4>
+
+			<p>
+				25MB
+				<br>
+				PDF
+			</p>
+
+			<div class="progress">
+				
+				<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 49%;">
+					49%
 				</div>
 				
 			</div>
