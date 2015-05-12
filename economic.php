6c614514118c2f224b011a8129403fb5b9dae0a2
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-12 14:37:12 +0200

    login og SOAP

--- economic.php
+++ economic.php
@@ -1,32 +1,18 @@
 <?php
 
-// $url = "https://restapi.e-conomic.com/customers";
+	$wsdlUrl = 'https://api.e-conomic.com/secure/api1/EconomicWebservice.asmx?WSDL';
 
+	$client = new SoapClient($wsdlUrl, array("trace" => 1, "exceptions" => 1));    		
+	$client->ConnectWithToken(
+		array(
+			'token' 	=> $_REQUEST['token'],
+			'appToken'  => '9fvq9KzQuegRUXO1SAQ053byEAj7L4OnolyYqC9hXX81'
+		)
+	);
 
-// $agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
-
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// curl_setopt($ch, CURLOPT_VERBOSE, true);
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-// curl_setopt($ch, CURLOPT_URL,$url);
-// curl_setopt_array($ch, array(
-//	 CURLOPT_HTTPHEADER  	=> array(
-//	 	'AppID: z5D_fdywpE3eTjq5R9yUqWabK5BKaq_lxaDp2ksMIA81',
-//	 	'AccessID : 9fvq9KzQuegRUXO1SAQ053byEAj7L4OnolyYqC9hXX81',
-//	 	'accept: application/json'
-//	 ),
-//	 CURLOPT_RETURNTRANSFER  => true,
-//	 CURLOPT_VERBOSE	 	=> 1
-// ));
-
-// $response = curl_exec($ch);
-
-// echo "<pre>";
-// print_r(json_decode($response));
-// echo "</pre>";
-
+appId: "z5D_fdywpE3eTjq5R9yUqWabK5BKaq_lxaDp2ksMIA81",
+accessId: "9fvq9KzQuegRUXO1SAQ053byEAj7L4OnolyYqC9hXX81",
+accept: "application/json"
 ?>
 
 <!DOCTYPE html>
@@ -43,46 +29,30 @@
 
 			$(document).ready(function()
 			{
-				$.ajax({
-					url: "https://restapi.e-conomic.com/customers",
-					dataType: "json",
-					headers:{
-						appId: "z5D_fdywpE3eTjq5R9yUqWabK5BKaq_lxaDp2ksMIA81",
-						accessId: "9fvq9KzQuegRUXO1SAQ053byEAj7L4OnolyYqC9hXX81",
-						accept: "application/json"
-					},
-					contentType: 'application/json',
-					data: {
-					    "currency":"DKK",
-					    "customerGroup":{
-					        "customerGroupNumber":2,
-					        "self":"https://restapi.e-conomic.com/customer-groups/2"
-					    },
-					    "vatZone":{
-					        "vatZoneNumber":1,
-					        "self":"https://restapi.e-conomic.com/vat-zones/1"
-					    },
-					    "name":"Leif",
-					    "paymentTerms":{
-					        "paymentTermsNumber":2,
-					        "paymentTermsType":0,
-					        "self":"https://restapi.e-conomic.com/payment-terms/2"
-					    },
-					    "balance":0.00
-					},
-					type: "POST",
-					success: function(data, textStatus, jqXHR)
-					{
-						console.log(data);
-					},
-					error: function(jqXHR, textStatus, errorThrown)
-					{
-						console.log(jqXHR.responseText);
-					}
-				});
-			});
-
-
+			// 	$.ajax({
+			// 		url: "https://restapi.e-conomic.com/PAYMENT-TERMS",
+			// 		dataType: "json",
+			// 		headers:{
+			// 			appId: "z5D_fdywpE3eTjq5R9yUqWabK5BKaq_lxaDp2ksMIA81",
+			// 			accessId: "9fvq9KzQuegRUXO1SAQ053byEAj7L4OnolyYqC9hXX81",
+			// 			accept: "application/json"
+			// 		},
+			// 		contentType: 'application/json',
+			// 		data: {
+			// 		    "name": "test betalingstype",
+			// 			"paymentTermsType": "net"
+			// 		},
+			// 		type: "POST",
+			// 		success: function(data, textStatus, jqXHR)
+			// 		{
+			// 			console.log(data);
+			// 		},
+			// 		error: function(jqXHR, textStatus, errorThrown)
+			// 		{
+			// 			console.log(jqXHR.responseText);
+			// 		}
+			// 	});
+			// });
 
 		</script>
 
