6d7fc67f946670c069abce4fbd9a0a0e3a3f76b9
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-14 19:35:49 +0200

    Flow and shit

--- js/custome-script.js
+++ js/custome-script.js
@@ -1,5 +1,65 @@
 $(document).ready(function()
 {
+	function setVisibleStep(stepNumber)
+	{
+		var productInfomationFlow = $('div.band.product-infomation');
 
+		productInfomationFlow.attr('data-show-step-no', stepNumber);
+		productInfomationFlow.find('header.flow-overview div a').removeClass('focus');
+		productInfomationFlow.find('header.flow-overview div:nth-child('+ (stepNumber+1) +') > a').addClass('focus');
 
+		productInfomationFlow.find('div.steps div.step').removeClass('show');
+		productInfomationFlow.find('div.steps div.step:nth-child('+ stepNumber +')').addClass('show');
+	}
+
+	$('div.band.product-infomation header.flow-overview div a')
+		.on('click', function(event)
+		{	
+			event.preventDefault();
+			
+			var stepNumber = parseInt($(this).parent().index());
+			setVisibleStep(stepNumber);
+		});
+
+	$('div.band.product-infomation footer button:not([type="submit"])')
+		.on('click', function(event)
+		{
+			event.preventDefault();
+			
+			var productInfomationFlow = $('div.band.product-infomation');
+
+			var action 			= $(this).attr('data-flow-action');
+			var numberOfSteps 	= productInfomationFlow.find('div.steps div.step').length;
+			var stepNumber 		= parseInt(productInfomationFlow.attr('data-show-step-no'));
+
+			switch(action)
+			{
+				case 'next':
+				
+					if(stepNumber < numberOfSteps)
+						setVisibleStep(stepNumber+1);
+					
+					break;
+
+				case 'back':
+					if(stepNumber > 1)
+						setVisibleStep(stepNumber-1);
+	
+					break;
+				
+				case 'add-to-cart':
+					
+					break;
+				
+				default:
+					/*do nothing*/
+			}
+		});
+
+	$('form').on('submit', function(event)
+	{
+		event.preventDefault();
+		
+		console.log('submit');
+	});
 });
