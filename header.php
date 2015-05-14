02da6787713f56df81b2c23e7075ae99b5dcf7d8
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-14 12:48:25 +0200

    clean slate

--- header.php
+++ header.php
@@ -1,26 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="da">
+	
 	<head>
+
 		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Nord Grafisk</title>
- 
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/customstyle.css" rel="stylesheet">
-		<link href="css/upload-fane-style.css" rel="stylesheet">
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/custome-style.css"> 
 
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<!--<script type="text/javascript" src="js/bootstrap.js"></script>-->
-		<script type="text/javascript" src="js/script.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="js/custome-script.js"></script>
 
- 		<script src="js/upload-script.js" type="text/javascript"></script>
-		
-		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-		<!--[if lt IE 9]>
-			<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-			<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
 	</head>
+	
+	<body>
--- index.php
+++ index.php
@@ -1,57 +1,9 @@
-<?php
-?>
+<?php require 'header.php'; ?>
 
-<!DOCTYPE html>
-<html lang="da">
-	
-	<head>
+<!--
 
-		<meta charset="utf-8">
-		<title>Nord grafisk</title>
+	Your farts here
 
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+-->		
 
-		<script src="js/upload-script.js" type="text/javascript"></script>
-
-		<style type="text/css">
-
-			div.upload-progress-bar{
-				background-color: lightgray;
-				border:solid 1px gray;
-				border-radius: 4px;
-				overflow: hidden;
-				width: 300px;
-			}
-
-			div.upload-progress-bar span.progress{
-				background-image: url(img/progress.gif);
-				display: block;
-				width: 0%;
-				-webkit-transition: width 8s; /* Safari */
-			    transition: width 8s;
-			    font-weight: bold;
-			}
-
-		</style>
-
-	</head>
-	
-	<body>
-
-		<form action="upload.php" method="post" class="file-upload" enctype="multipart/form-data">
-		
-			Select image to upload:
-			<input type="file" name="file" multiple>
-			<input type="submit" value="Upload Image" name="submit" >
-		
-		</form>
-
-		<br>
-
-		<div class="upload-progress-bar">
-			<span class="progress">0%</span>
-		</div>
-
-	</body>
-
-</html>
+<?php require 'footer.php'; ?>
