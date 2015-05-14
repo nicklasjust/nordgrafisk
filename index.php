2812e2066f0ebd022dfd63145d1a27b2d7021300
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-14 13:40:25 +0200

    Hello NG

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php require 'header.php'; ?>
+	
+	<div class="container">
 
-<!--
+		Hello Nord Grafisk
 
-	Your farts here
+		<!--
 
--->		
+			Your farts here
+
+		-->		
+
+	</div>
 
 <?php require 'footer.php'; ?>
