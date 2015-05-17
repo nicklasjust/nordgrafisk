f9eb90dbd6786d378416153f915ae3ac93b23419
CHelding <christianheldingsorensen@Christians-MacBook-Pro.local>
2015-05-17 16:08:34 +0200

    Tilføjet modalz til knapper i toppen

--- index.php
+++ index.php
@@ -17,11 +17,85 @@
 				<div class="col-md-4">
 
 					<div class="col-md-6">
-						<button type="button" class="btn btn-default">Indhent tilbud</button>
+						<!-- Button trigger modal - Indhent tilbud -->
+						<button type="button" class="btn btn-default" data-toggle="modal" data-target="#indhenttilbud">
+						  	Indhent tilbud
+						</button>
+						
+						<!-- Modal - Indhent tilbud -->
+						<div class="modal fade" id="indhenttilbud" tabindex="-1" role="dialog" aria-labelledby="tilbudLabel" aria-hidden="true">
+							  <div class="modal-dialog">
+								    <div class="modal-content">
+									      <div class="modal-header">
+									        	<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+									        	<h4 class="modal-title" id="tilbudLabel">Indtast referencenummer</h4>
+									      </div>
+									      <div class="modal-body">
+										        <form class="form-horizontal">
+													  <div class="form-group">
+													  		<label for="inputRefnr" class="col-sm-2 control-label">Ref.nr.</label>
+													    	<div class="col-sm-10">
+													      	<input type="text" class="form-control" id="inputRefnr" placeholder="Referencenummer">
+													    	</div>
+													  </div>
+												</form>
+									      </div>
+									      <div class="modal-footer">
+									        	<button type="button" class="btn btn-default" data-dismiss="modal">Annullér</button>
+									        	<button type="button" class="btn btn-primary">Indhent tilbud</button>
+									      </div>
+								    </div>
+							  </div>
+						</div>
+						
 					</div>
 
 					<div class="col-md-6">
-						<button type="button" class="btn btn-default">Log ind</button>
+						
+						<!-- Button trigger modal - Log ind -->
+						<button type="button" class="btn btn-default" data-toggle="modal" data-target="#login">
+						  	Log ind
+						</button>
+						
+						<!-- Modal - Log ind -->
+						<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="loginLabel" aria-hidden="true">
+							<div class="modal-dialog">
+						
+							    <div class="modal-content">
+						
+							      	<div class="modal-header">
+						
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">
+							        &times;</span></button>
+							        <h4 class="modal-title" id="loginLabel">Login-oplysninger</h4>
+							      
+							     	 </div>
+							      
+							      	<div class="modal-body">
+							      
+							        	<form class="form-horizontal">
+									  		<div class="form-group">
+									    		<label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+									    		<div class="col-sm-10">
+									      			<input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+									    		</div>
+									  		</div>
+									  		<div class="form-group">
+									    		<label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+									    		<div class="col-sm-10">
+									      			<input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+									    		</div>
+									  		</div>
+										</form>
+							      	</div>
+							      	<div class="modal-footer">
+							        	<button type="button" class="btn btn-default" data-dismiss="modal">Annullér</button>
+							        	<button type="button" class="btn btn-primary">Log ind</button>
+							      	</div>
+							    </div>
+							</div>
+						</div>
+						
 					</div>
 				
 				</div>
