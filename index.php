70a4e472cf91a10a123baafbcc0e685e5492cc75
CHelding <christianheldingsorensen@Christians-MacBook-Pro.local>
2015-05-19 16:43:10 +0200

    Formatfanen er funktionel(ish)

--- index.php
+++ index.php
@@ -321,63 +321,175 @@
 								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 								   Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>	
 
-								<div class="row">
+								<div class="row parent_format">
 
-									<div class="col-md-4">
-										
-										<div class="size hide-other">
+									<div class="row format poster col-md-12"> <!-- Begin - Format - Poster -->
 									
-											<h4>Størrelse:</h4>
-
-											<select name="size" class="form-control">
-										
-												<option>A0</option>
-												<option>A1</option>
-											 	<option>A2</option>
-											 	<option>A3</option>
-												<option>A4</option>
-												<option>A5</option>
-												<option>B0</option>
-												<option>B1</option>
-											 	<option>B2</option>
-											 	<option>B3</option>
-												<option>B4</option>
-												<option>B5</option>
-												<option>Andet</option>
+										<div class="col-md-4">
 											
-											</select>
-
-											<div class="other">
-
-												<input type="text" class="form-control" placeholder="Længde">
-												X
-												<input type="text" class="form-control" placeholder="Bredde">
-												mm 
-
+											<div class="size hide-other">
+										
+												<h4>Størrelse:</h4>
+	
+												<select name="size" class="form-control">
+											
+													<option>A0</option>
+													<option>A1</option>
+												 	<option>A2</option>
+												 	<option>A3</option>
+													<option>A4</option>
+													<option>A5</option>
+													<option>B0</option>
+													<option>B1</option>
+												 	<option>B2</option>
+												 	<option>B3</option>
+													<option>B4</option>
+													<option>B5</option>
+													<option>Andet</option>
+												
+												</select>
+	
+												<div class="other">
+	
+													<input type="text" class="form-control" placeholder="Længde">
+													X
+													<input type="text" class="form-control" placeholder="Bredde">
+													mm 
+	
+												</div>
+	
 											</div>
-
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+											<h4>Type:</h4>
+	
+												<select name="type" class="form-control">
+											
+													<option>Farve</option>
+													<option>Farve m. gloss</option>
+													<option>Sort/Hvid</option>
+												 	<option>Sort/Hvid m. gloss</option>
+												
+												</select>
+	
 										</div>
 									
-									</div>
-
-									<div class="col-md-4">
+									</div> <!-- End - Format - Poster -->
 									
-									</div>
-
-									<div class="col-md-4">
+									<div class="row format roll-up col-md-12"> <!-- Begin - Format - Roll-up -->
 									
-										<h4>Type:</h4>
-
-											<select name="type" class="form-control">
-										
-												<option>Farve</option>
-												<option>Farve m. gloss</option>
-												<option>Sort/Hvid</option>
-											 	<option>Sort/Hvid m. gloss</option>
+										<div class="col-md-4">
 											
-											</select>
-
-									</div>
+											<div class="size hide-other">
+										
+												<h4>Størrelse:</h4>
+	
+												<select name="size" class="form-control">
+											
+													<option>80 x 220</option>
+													<option>85 x 250</option>
+													<option>Andet</option>
+												
+												</select>
+	
+												<div class="other">
+	
+													<input type="text" class="form-control" placeholder="Længde">
+													X
+													<input type="text" class="form-control" placeholder="Bredde">
+													mm 
+	
+												</div>
+	
+											</div>
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+											<h4>Type:</h4>
+	
+												<select name="type" class="form-control">
+											
+													<option>PVC m. block</option>
+													<option>ECO Tex Polyester</option>
+													<option>PVC</option>
+												
+												</select>
+	
+										</div>
+									
+									</div> <!-- End - Format - Roll-up -->
+									
+									<div class="row format banner col-md-12"> <!-- Begin - Format - Banner -->
+									
+										<div class="col-md-4">
+											
+											<div class="size hide-other">
+										
+												<h4>Størrelse:</h4>
+	
+												<select name="size" class="form-control">
+											
+													<option>A0</option>
+													<option>A1</option>
+												 	<option>A2</option>
+												 	<option>A3</option>
+													<option>A4</option>
+													<option>A5</option>
+													<option>B0</option>
+													<option>B1</option>
+												 	<option>B2</option>
+												 	<option>B3</option>
+													<option>B4</option>
+													<option>B5</option>
+													<option>Andet</option>
+												
+												</select>
+	
+												<div class="other">
+	
+													<input type="text" class="form-control" placeholder="Længde">
+													X
+													<input type="text" class="form-control" placeholder="Bredde">
+													mm 
+	
+												</div>
+	
+											</div>
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+										</div>
+	
+										<div class="col-md-4">
+										
+											<h4>Type:</h4>
+	
+												<select name="type" class="form-control">
+											
+													<option>Polyester 210g stofbanner</option>
+													<option>PVC 440g presenning</option>
+													<option>Mesh 420g stilladsbanner</option>
+												
+												</select>
+	
+										</div>
+									
+									</div> <!-- End - Format - Banner -->									
 
 								</div>
 
