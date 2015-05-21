c9223b39a1ac9fc99cf25121305677da8dc8bea8
nicklasjust <nicklasjust@Nicklass-MacBook-Pro.local>
2015-05-21 08:57:34 +0200

    ny e-conomic

--- economic.php
+++ economic.php
@@ -5,7 +5,7 @@
 	$client = new SoapClient($wsdlUrl, array("trace" => 1, "exceptions" => 1));    		
 	$client->ConnectWithToken(
 		array(
-			'token' 	=> 'nsfG0GFUMWUr10gt-_9v3wyKrxVpEcXxe2mDFHgHPus1',
+			'token' 	=> 'V17qsgk6uR-6MfrNkTdgRhydwNLTOkCXNcF7R588GMs1',
 			'appToken'  => 'dciTAm_Tp-pPujYOxcNhhB9QJin7ZtB3rd6qGH2-b6Q1'
 		)
 	);
