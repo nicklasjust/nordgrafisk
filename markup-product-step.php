f81f6ec0d76dcb2f6b8e509b1492394863e26483
TobiasEmilHarbo <tobiasharbo@gmail.com>
2015-05-21 10:50:19 +0200

    product step

--- markup-product-step.php
+++ markup-product-step.php
@@ -9,56 +9,161 @@
     	Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 	</p>
 	
-	<div class="row" id="firstRow">
-		<label class="col-md-2">
-				<img src="images/poster_icon.png" alt="Poster" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/rollup_icon.png" alt="Roll-up" class="img-rounded">
-			</label>
+	<div class="products">
 
+		<div class="row">
 			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-	</div>
-	
-	<div class="row" id="secondRow">
-		<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
-			
-			<label class="col-md-2">
-				<img src="images/1.png" alt="Andet" class="img-rounded">
-			</label>
+			<div class="product col col-md-2">
+
+				<input type="radio" id="product-poster" name="product" value="posters"/>
+
+				<label for="product-poster">
+
+					<img src="images/poster_icon.png" alt="Poster" class="img-rounded">
+
+				</label>
+
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-roll-up">
+
+					<img src="images/rollup_icon.png" alt="Roll-up" class="img-rounded">
+
+				</label>
+
+				<input type="radio" id="product-roll-up" name="product" value="roll-up"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-1">
+
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+
+				</label>
+
+				<input type="radio" id="product-1" name="product" value="andet"/>
+
+			</div>
+
+			<div class="product col col-md-2">
+
+				<label for="product-2">
+
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+
+				</label>
+
+				<input type="radio" id="product-2" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-3">
+
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+
+				</label>
+
+				<input type="radio" id="product-3" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-4">
+
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+
+				</label>
+
+				<input type="radio" id="product-4" name="product" value="andet"/>
+
+			</div>
+
 		</div>
+		
+		<div class="row">
+				
+			<div class="product col col-md-2">
+
+				<label for="product-5">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-5" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-6">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-6" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-7">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-7" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-8">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-8" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-9">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-9" name="product" value="andet"/>
+
+			</div>
+				
+			<div class="product col col-md-2">
+
+				<label for="product-10">
+				
+					<img src="images/1.png" alt="Andet" class="img-rounded">
+				
+				</label>
+
+				<input type="radio" id="product-10" name="product" value="andet"/>
+
+			</div>
+		
+		</div>
+
+	</div>
+
 </div>
